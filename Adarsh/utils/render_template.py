d01e3a1cc3c2c36ed77c0a2c71de90e5d604e787
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-12-10 22:14:16 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -50,8 +50,8 @@ async def render_page(id, secure_hash):
 </p>
 <center>
     <h2>
-        <a href="https://telegram.dog/+kc6bYRCsWdlhOTI1">
-            <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="Rkbotz" width="150" height="75">
+        <a href="https://telegram.dog/VS_bots">
+            <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="VSbots" width="150" height="75">
         </a>
     </h2>
 </center>
